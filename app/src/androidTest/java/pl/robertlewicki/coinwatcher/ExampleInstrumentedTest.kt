3864ece7dbbd71b580a55d3742d34bf9823111b9
rlewicki <zguba44@gmail.com>
2017-07-27 21:58:40 +0200

    Converting tests from Java to Kotlin

---
+++ app/src/androidTest/java/pl/robertlewicki/coinwatcher/ExampleInstrumentedTest.kt
@@ -0,0 +1,21 @@
+package pl.robertlewicki.coinwatcher
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("pl.robertlewicki.coinwatcher", appContext.packageName)
+    }
+}
